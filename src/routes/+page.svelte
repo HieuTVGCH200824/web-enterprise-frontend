679e7f040b6f29512881bf4cdd11e1b6b213747b
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-02-11 23:08:11 +0700

    userbar fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import Searchbar from "../components/Searchbar.svelte";
 </script>
 <div class="from-violet-200 to-violet-800 bg-gradient-to-tr h-full w-full fixed flex items-center justify-center">
-    <div class="w-[97%] h-full md:mt-12 max-sm:w-full max-sm:h-full md:rounded-xl flex bg-card-indigo bg-opacity-50">
+    <div class="w-[97%] h-full md:mt-12 max-sm:w-full max-sm:h-full md:rounded-xl flex bg-card-indigo bg-opacity-50 relative">
         <Sidebar />
         <div class="w-full h-full flex flex-col">
             <Searchbar></Searchbar>
