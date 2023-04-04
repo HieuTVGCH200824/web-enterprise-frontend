27388e39f512a13743f33e534d3b5b4602839eb8
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-04-04 22:42:36 +0700

    google analytic

--- src/routes/(authorized)/+layout.svelte
+++ src/routes/(authorized)/+layout.svelte
@@ -8,6 +8,25 @@
 export let data:any;
 
 </script>
+
+<svelte:head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3F18XVREL6"></script>
+<script>
+ try {
+        if(typeof window !== 'undefined' 
+        && window) {
+            const googleAnalyticsId = 'G-3F18XVREL6'
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', googleAnalyticsId);
+        }
+    } catch(error) {
+		console.error("Failed to initialize Analytics")
+	}
+</script>
+</svelte:head>
 <div class="from-violet-200 to-violet-800 bg-gradient-to-tr h-full w-full fixed flex items-center justify-center">
     <div class="w-[97%] h-full md:mt-12 max-md:w-full sm:rounded-xl flex bg-card-indigo bg-opacity-50 relative">
         <Sidebar />
