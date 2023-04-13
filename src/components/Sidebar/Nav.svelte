fa0c2f250deafc268b0700496be88492317eb42a
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-04-13 14:15:54 +0700

    add animation

--- src/components/Sidebar/Nav.svelte
+++ src/components/Sidebar/Nav.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div class="w-full">
-    <a href="/{path}" class="{barState?" justify-start":"justify-center"}  duration-1000 ease-in-out max-md:justify-center p-4 flex rounded items-center hover:text-violet-400 sm:space-x-5 sm:h-10 hover:bg-black/50">
-        <div>
+    <a href="/{path}" class="{barState?" justify-start":"justify-center"}   max-md:justify-center p-4 flex rounded items-center hover:text-violet-400 sm:space-x-5 sm:h-10 hover:bg-black/50">
+        <div >
             <slot/>
         </div>
-        <p class="{barState?" visibile opacity-100" : "hidden invisible opacity-0"} max-md:invisible ">
+        <p class="{barState?"visibile opacity-100 translate-x-0 text-base" : "absolute w-0 h-0 invisible opacity-0 -translate-x-10"} ease-in-out duration-500 max-md:invisible ">
             {content}
         </p>
     </a>
