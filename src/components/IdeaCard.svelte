bcb76a492e19744ffe58e5e1dea2d1b40be426bf
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-04-15 14:00:02 +0700

    rescale ideacard bg

--- src/components/IdeaCard.svelte
+++ src/components/IdeaCard.svelte
@@ -14,8 +14,8 @@
 
 </script>
 <div class="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full h-48 rounded-3xl flex items-center flex-col justify-end shadow-2xl shadow-black bg-center bg-cover bg-no-repeat" style={idea.image?`background-image: url('${idea.image}')`:""}>
-    <div class="w-full h-[60%] rounded-b-3xl flex flex-col justify-end space-y-4 bg-card-indigo/70">
-        <div class=" flex flex-col pl-4 pt-2 w-full">
+    <div class="w-full rounded-b-3xl flex flex-col justify-end space-y-4 bg-card-indigo/70 min-h-fit">
+        <div class=" flex flex-col pl-4 pt-2 w-full min-h-fit">
             <p class="font-bold text-xl">
                 {idea.title}
             </p>
