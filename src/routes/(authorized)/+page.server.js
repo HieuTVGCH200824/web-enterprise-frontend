6ff19b5e01aeae873e4381aa6f86372b1c02a997
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-04-15 13:42:48 +0700

    reverse idea data

--- src/routes/(authorized)/+page.server.js
+++ src/routes/(authorized)/+page.server.js
@@ -11,7 +11,7 @@ export async function load({ locals,url }) {
 	const departments = await api.get('departments', locals.user.Token);
 	const allUser = await api.get('users', locals.user.Token);
 	const body = {
-		ideas: res.data,
+		ideas: res.data?.reverse(),
 		user: locals.user,	
 		comments: comments.data,
 		votes: votes.data,
