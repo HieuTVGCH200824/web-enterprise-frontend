573ab1be5114976f1dfa498b8897e6e5deb5f827
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-04-26 15:31:47 +0700

    sidebar fix

--- src/components/Button/LogoutButton.svelte
+++ src/components/Button/LogoutButton.svelte
@@ -3,16 +3,13 @@
     export let barState:boolean = false;
 </script>
 
-<form use:enhance method="POST" action="/?/logout" class="w-full">
-    <button class="{barState?"md:justify-start":"justify-center"} sm:h-10 max-md:justify-center w-full items-center max-w-full align-center max-sm:flex-col-reverse p-4 flex rounded hover:text-violet-400 sm:space-x-5 hover:bg-black/50">
-        <div>
-
+<form use:enhance method="POST" action="/?/logout" class="">
+    <button class="{barState?"":"justify-center"}  w-full max-md:justify-center p-4 flex rounded items-center hover:text-violet-400 sm:space-x-5 sm:h-10 hover:bg-black/50">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
             </svg>
-        </div>
-          <p class="{barState?"visibile opacity-100 translate-x-0 text-base" : "absolute w-0 h-0 invisible opacity-0 -translate-x-10"} ease-in-out duration-500 max-md:invisible whitespace-nowrap">
-            Log Out
+            <p class="{barState?"visibile opacity-100 translate-x-0 text-base w-0" : "absolute w-0 h-0 invisible opacity-0 -translate-x-10"} whitespace-nowrap ease-in-out duration-500 max-md:invisible ">
+                Log Out
         </p>
     </button>
 </form>
